7845426867e3b886d60408fad8665609baa2e4d9
Bryan Calderon <bcalderon@retailcompass.com>
2021-06-14 17:14:39 -0500

    Se limpia codigo y se dejan funcionalidades basicas para hacer un MVP

--- backend_agencia_viajes/app/models.py
+++ backend_agencia_viajes/app/models.py
@@ -3,31 +3,6 @@ from django.utils import timezone
 from rest_framework import serializers
 
 
-class Complemento(models.Model):
-    descripcion = models.TextField()
-    estado = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.descripcion
-
-
-class ComplementoDestino(models.Model):
-    precio = models.FloatField(blank=True)
-    estado = models.BooleanField(default=True)
-    complemento = models.ForeignKey("Complemento", on_delete=models.DO_NOTHING)
-    destino_turistico = models.ForeignKey("DestinoTuristico", on_delete=models.DO_NOTHING)
-
-
-class DestinoTuristico(models.Model):
-    nombre = models.CharField(max_length=100)
-    ciudad = models.ForeignKey("Ciudad", on_delete=models.DO_NOTHING)
-    created = models.DateTimeField(auto_now_add=True)
-    estado = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.nombre
-
-
 class Ciudad(models.Model):
     nombre = models.CharField(max_length=100)
     codigo = models.CharField(max_length=10)
@@ -54,51 +29,24 @@ class Pais(models.Model):
         return self.nombre
 
 
-# class ClientePlan(models.Model):
-#     plan = models.ForeignKey('Plan', on_delete=models.DO_NOTHING)
-#     cliente = models.ForeignKey('Cliente', on_delete=models.DO_NOTHING)
-#     complementos = models.ManyToManyField(ComplementoDestino)
-
-
 class Plan(models.Model):
     titulo = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True)
     imagen = models.CharField(max_length=455)
-    fecha_ida = models.DateTimeField(null=True)
-    fecha_regreso = models.DateTimeField(null=True)
-    rating = models.IntegerField(null=True)
-    precio = models.FloatField(null=True)
-    destinos = models.ManyToManyField(DestinoTuristico, through='DestinoHotel', through_fields=('plan', 'destino'))
 
     def __str__(self):
         return self.titulo
 
 
-class DestinoHotel(models.Model):
-    plan = models.ForeignKey("Plan", on_delete=models.DO_NOTHING)
-    destino = models.ForeignKey("DestinoTuristico", on_delete=models.DO_NOTHING)
-    hotel = models.ForeignKey("Hotel", on_delete=models.DO_NOTHING, null=True)
-    hotel_tipo_habitacion = models.ForeignKey("HotelTipoHabitacion", on_delete=models.DO_NOTHING, null=True)
+class FechaPlan(models.Model):
+    fecha_ida = models.DateTimeField(null=True)
+    fecha_regreso = models.DateTimeField(null=True)
+    precio = models.FloatField(null=True)
+    plan = models.ForeignKey('Plan', related_name='fechas', on_delete=models.DO_NOTHING)
 
 
-class HotelTipoHabitacion(models.Model):
-    precio = models.FloatField()
-    capacidad = models.IntegerField()
-    tipo_habitacion = models.ForeignKey("TipoHabitacion", on_delete=models.DO_NOTHING)
-    hotel = models.ForeignKey("Hotel", on_delete=models.DO_NOTHING)
-
-
-class TipoHabitacion(models.Model):
+class Habitacion(models.Model):
     descripcion = models.CharField(max_length=100)
-    estado = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.descripcion
-
-
-class Servicio(models.Model):
-    descripcion = models.CharField(max_length=100)
-    estado = models.BooleanField(default=True)
 
     def __str__(self):
         return self.descripcion
@@ -112,26 +60,16 @@ class Hotel(models.Model):
     imagen = models.CharField(max_length=455)
     url = models.CharField(max_length=255)
     ciudad = models.ForeignKey("Ciudad", on_delete=models.DO_NOTHING)
-    categoria = models.ForeignKey("Categoria", on_delete=models.DO_NOTHING, null=True)
-    servicios = models.ManyToManyField(Servicio, through="HotelServicio", through_fields=('hotel', 'servicio'))
-    tipo_habitaciones = models.ManyToManyField(TipoHabitacion, through="HotelTipoHabitacion",
-                                               through_fields=('hotel', 'tipo_habitacion'))
 
     def __str__(self):
         return self.nombre
 
 
-class HotelServicio(models.Model):
+class HotelHabitacion(models.Model):
     precio = models.FloatField()
-    servicio = models.ForeignKey("Servicio", on_delete=models.DO_NOTHING)
+    capacidad = models.IntegerField()
+    habitacion = models.ForeignKey("Habitacion", on_delete=models.DO_NOTHING)
     hotel = models.ForeignKey("Hotel", on_delete=models.DO_NOTHING)
-
-
-class Categoria(models.Model):
-    descripcion = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.descripcion
 
 
 class Cliente(models.Model):
@@ -150,20 +88,18 @@ class Cliente(models.Model):
         return "{} {}".format(self.nombres, self.apellidos)
 
 
-class Promocion(models.Model):
-    titulo = models.CharField(max_length=100)
-    descripcion = models.TextField(blank=True)
-    imagen = models.CharField(max_length=455)
-    plan = models.ForeignKey("Plan", on_delete=models.DO_NOTHING)
-
-
-class Compras(models.Model):
+class Factura(models.Model):
     plan = models.ForeignKey("Plan", on_delete=models.DO_NOTHING)
     cliente = models.ForeignKey("Cliente", on_delete=models.DO_NOTHING)
-    ciudad = models.ForeignKey("Ciudad", on_delete=models.DO_NOTHING)
-    fecha_ida = models.DateField()
-    fecha_regreso = models.DateField()
-    precio = models.FloatField(null=True)
+    fechas = models.ForeignKey("FechaPlan", on_delete=models.DO_NOTHING)
+    valor_pagar = models.FloatField()
     hotel = models.ForeignKey("Hotel", on_delete=models.DO_NOTHING, null=True)
-    servicios = models.ManyToManyField(Servicio, null=True)
-    tipo_habitacion = models.ForeignKey("TipoHabitacion", on_delete=models.DO_NOTHING, null=True)
+    hotel_habitacion = models.ForeignKey("HotelHabitacion", on_delete=models.DO_NOTHING, null=True)
+
+
+class Review(models.Model):
+    comentario = models.CharField(max_length=455)
+    created = models.DateTimeField(auto_created=True, auto_now=True, editable=False)
+    calificacion = models.IntegerField(null=True)
+    plan = models.ForeignKey("Plan", on_delete=models.DO_NOTHING)
+    cliente = models.ForeignKey("Cliente", on_delete=models.DO_NOTHING)
--- backend_agencia_viajes/app/views.py
+++ backend_agencia_viajes/app/views.py
@@ -30,10 +30,31 @@ class PlanViewSet(viewsets.ModelViewSet):
     queryset = Plan.objects.all()
     serializer_class = PlanSerializer
 
+    def list(self, request, *args, **kwargs):
+        fecha_plan_list = FechaPlanSerializer.get_plans_by_future_dates(self)
+        planes = [fp.plan for fp in fecha_plan_list]
+        serializer = PlanSerializer(planes, many=True, context={'request': request})
+        return Response(serializer.data)
 
-class DestinoTuristicoViewSet(viewsets.ModelViewSet):
-    queryset = DestinoTuristico.objects.all()
-    serializer_class = DestinoTuristicoSerializer
+    def retrieve(self, request, *args, **kwargs):
+        plan_id = kwargs.get("pk")
+        plan = Plan.objects.filter(id=plan_id).first()
+        if plan:
+            dates = FechaPlanSerializer.get_by_plan(self, plan_id)
+            if dates and dates.count() > 0:
+                serializer = PlanSerializer(plan, context={'request': request})
+                return Response(serializer.data)
+
+            return Response({'message': 'Plan with ID {} is not available'.format(plan_id)},
+                            status=status.HTTP_404_NOT_FOUND)
+
+        return Response({'message': 'Plan with ID {} not found'.format(plan_id)},
+                        status=status.HTTP_404_NOT_FOUND)
+
+
+class FechaPlanViewSet(viewsets.ModelViewSet):
+    queryset = FechaPlan.objects.all()
+    serializer_class = FechaPlanSerializer
 
 
 class CiudadViewSet(viewsets.ModelViewSet):
@@ -45,7 +66,7 @@ class DepartamentoViewSet(viewsets.ModelViewSet):
     queryset = Departamento.objects.all()
     serializer_class = DepartamentoSerializer
 
-    @action(detail=True, methods=['get'], url_path='cities')
+    @action(detail=False, url_path='cities')
     def getCitiesByDepartment(self, request, pk=None):
         cities = Ciudad.objects.filter(departamento=pk)
         serializer = CiudadSerializer(cities, many=True, context={'request': request})
@@ -55,27 +76,6 @@ class DepartamentoViewSet(viewsets.ModelViewSet):
 class PaisViewSet(viewsets.ModelViewSet):
     queryset = Pais.objects.all()
     serializer_class = PaisSerializer
-
-
-class ComplementoViewSet(viewsets.ModelViewSet):
-    queryset = Complemento.objects.all()
-    serializer_class = ComplementoSerializer
-
-
-class ComplementoDestinoViewSet(viewsets.ModelViewSet):
-    queryset = ComplementoDestino.objects.all()
-    serializer_class = ComplementoDestinoSerializer
-
-
-class DestinoHotelViewSet(viewsets.ModelViewSet):
-    queryset = DestinoHotel.objects.all()
-    serializer_class = DestinoHotelSerializer
-
-
-#
-# class ClientePlanViewSet(viewsets.ModelViewSet):
-#     queryset = ClientePlan.objects.all()
-#     serializer_class = ClientePlanSerializer
 
 
 class ClienteViewSet(viewsets.ModelViewSet):
@@ -93,25 +93,20 @@ class ClienteViewSet(viewsets.ModelViewSet):
                             status=status.HTTP_404_NOT_FOUND)
 
 
-class PromocionViewSet(viewsets.ModelViewSet):
-    queryset = Promocion.objects.all()
-    serializer_class = PromocionSerializer
-
-
-class HotelTipoHabitacionViewSet(viewsets.ModelViewSet):
-    queryset = HotelTipoHabitacion.objects.all()
-    serializer_class = HotelTipoHabitacionSerializer
+class HotelHabitacionViewSet(viewsets.ModelViewSet):
+    queryset = HotelHabitacion.objects.all()
+    serializer_class = HotelHabitacionSerializer
 
     @action(detail=False, methods=['get'], url_path='getByHotel/(?P<hotel_pk>[^/.]+)')
     def getByHotel(self, request, hotel_pk=None):
-        hth = HotelTipoHabitacion.objects.filter(hotel=hotel_pk)
-        serializer = HotelTipoHabitacionSerializer(hth, many=True, context={'request': request})
+        hth = HotelHabitacion.objects.filter(hotel=hotel_pk)
+        serializer = HotelHabitacionSerializer(hth, many=True, context={'request': request})
         return Response(serializer.data)
 
 
-class TipoHabitacionViewSet(viewsets.ModelViewSet):
-    queryset = TipoHabitacion.objects.all()
-    serializer_class = TipoHabitacionSerializer
+class HabitacionViewSet(viewsets.ModelViewSet):
+    queryset = Habitacion.objects.all()
+    serializer_class = HabitacionSerializer
 
 
 class HotelViewSet(viewsets.ModelViewSet):
@@ -125,30 +120,9 @@ class HotelViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
 
-class HotelServicioViewSet(viewsets.ModelViewSet):
-    queryset = HotelServicio.objects.all()
-    serializer_class = HotelServicioSerializer
-
-    @action(detail=False, methods=['get'], url_path='getByHotel/(?P<hotel_pk>[^/.]+)')
-    def getByHotel(self, request, hotel_pk=None):
-        hs = HotelServicio.objects.filter(hotel=hotel_pk)
-        serializer = HotelServicioSerializer(hs, many=True, context={'request': request})
-        return Response(serializer.data)
-
-
-class ServicioViewSet(viewsets.ModelViewSet):
-    queryset = Servicio.objects.all()
-    serializer_class = ServicioSerializer
-
-
-class CategoriaViewSet(viewsets.ModelViewSet):
-    queryset = Categoria.objects.all()
-    serializer_class = CategoriaSerializer
-
-
-class CompraViewSet(viewsets.ModelViewSet):
-    queryset = Compras.objects.all()
-    serializer_class = CompraSerializer
+class FacturaViewSet(viewsets.ModelViewSet):
+    queryset = Factura.objects.all()
+    serializer_class = FacturaSerializer
 
     def create(self, request, *args, **kwargs):
         referidos = request.data['referidos']
@@ -182,20 +156,31 @@ class CompraViewSet(viewsets.ModelViewSet):
             if request.data['hotel']:
                 hotel = Hotel.objects.filter(id=request.data['hotel']['id']).first()
 
-            tipo_habitacion = None
-            if request.data['tipo_habitacion']:
-                tipo_habitacion = TipoHabitacion.objects.filter(
-                    id=request.data['tipo_habitacion']['tipo_habitacion']['id']).first()
+            hotel_habitacion = None
+            if request.data['hotel_habitacion']:
+                hotel_habitacion = HotelHabitacion.objects.filter(
+                    id=request.data['hotel_habitacion']['hotel_habitacion']['id']).first()
 
-            compra = Compras(
+            factura = Factura(
                 plan=plan,
                 cliente=main_client,
                 ciudad=ciudad,
-                fecha_ida=request.data.get("fecha_ida"),
-                fecha_regreso=request.data.get("fecha_regreso"),
+                # fecha_ida=request.data.get("fecha_ida"),
+                # fecha_regreso=request.data.get("fecha_regreso"),
                 precio=request.data.get("precio"),
                 hotel=hotel,
-                tipo_habitacion=tipo_habitacion
+                hotel_habitacion=hotel_habitacion
             )
-            compra.save()
+            factura.save()
             return Response(status=HTTPStatus.CREATED)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    @action(detail=False, methods=['get'], url_path='plan/(?P<plan_id>[^/.]+)')
+    def get_reviews_by_plan(self, request, plan_id=None):
+        reviews = Review.objects.filter(plan=plan_id)
+        serializer = ReviewSerializer(reviews, many=True, context={'request': request})
+        return Response(serializer.data)
